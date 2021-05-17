96fabd8309845931164d810497e1a8482c0d5cd7
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-05-17 09:34:59 -0400

    Create SuccessView.php

---
+++ codeingniter/app/Views/SuccessView.php
@@ -0,0 +1,12 @@
+<html>
+<head> 
+</head>
+<body>
+<div class="container">
+    <div class="row text-center">
+        <div class="col-sm-6 col-sm-offset-3">
+        <br><br> <h2 style="color:#0fad00">Success</h2>
+        </div>
+    </div>
+</body>
+</html>
