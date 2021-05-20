810989d1888212f88933b3b3a75919f66cc15c2e
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-05-20 15:29:05 -0400

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -5,7 +5,8 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00">Success</h2>
+         <br><br> <center><h2 style="color:#0fad00">$this->request->getVar('user_id')</h2></center>
+         <br><br> <center><h2 style="color:#0fad00">$this->request->getVar('password')</h2></center>
         </div>
     </div>
 </body>
